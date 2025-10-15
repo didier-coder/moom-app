eaa9ac0ce66ed1fafb1dffb769688ccf0d5fee51
MooM <119953471+didier-coder@users.noreply.github.com>
2025-10-15 07:49:54 +0200

    Update logger.js

--- backend/utils/logger.js
+++ backend/utils/logger.js
@@ -1,1 +1,26 @@
+import winston from "winston";
+
+const { combine, timestamp, colorize, printf } = winston.format;
+
+const logFormat = printf(({ level, message, timestamp }) => {
+  return `[${timestamp}] ${level}: ${message}`;
+});
+
+const logger = winston.createLogger({
+  level: "info",
+  format: combine(
+    colorize(),
+    timestamp({ format: "YYYY-MM-DD HH:mm:ss" }),
+    logFormat
+  ),
+  transports: [
+    new winston.transports.Console(),
+    new winston.transports.File({
+      filename: "logs/error.log",
+      level: "error",
+    }),
+  ],
+});
+
+export default logger;
 
