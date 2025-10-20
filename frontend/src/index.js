461114fce29204f6a175f5fd5e6d39ace3fc3c8d
MooM <119953471+didier-coder@users.noreply.github.com>
2025-10-20 09:43:42 +0200

    Update index.js

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,9 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-document.body.style.backgroundColor = "#bad5b7";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
+document.body.style.backgroundColor = "#bad5b7";
 root.render(
   <React.StrictMode>
     <App />
