07424ee1389fad83ffadf190efc8fcad8538ede2
MooM <119953471+didier-coder@users.noreply.github.com>
2025-10-20 15:06:25 +0200

    Update Reservation.js

--- frontend/src/Reservation.js
+++ frontend/src/Reservation.js
@@ -409,7 +409,7 @@ const responsiveContainer = {
 const cardStyle = {
   width: "100%",
   maxWidth: "650px",
-  background: "transparent",
+  background: "#fff",
   borderRadius: "20px",
   boxShadow: "0 10px 30px rgba(0,0,0,0.1)",
   padding: "2rem 2.5rem",
